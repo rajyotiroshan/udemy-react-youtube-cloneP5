14f89685833e8b52b8412b7b4f4691dcb9f6adf3
rajan <rajangreedge@gmail.com>
2019-03-14 17:01:41 +0530

    customize axios with baseurL and params

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,11 @@
 const KEY='AIzaSyDcaEQ7oXyHW7pPeBOsR6k4Rj-oa4eKqp0';
+const axios = require('axios');
+
+export default axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResult: 5,
+        key: KEY
+    }
+});
