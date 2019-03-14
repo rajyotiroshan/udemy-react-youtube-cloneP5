b487b14402600915021c7d12f2e19f4f5feaa0db
rajan <rajangreedge@gmail.com>
2019-03-14 16:43:29 +0530

    handle state change for input

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
+    
     render(){
         return(
             <div className="ui container">
